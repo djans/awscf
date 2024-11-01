f60c6f2ed3bf5015863d1f4c9ef7858e8f1c6363
JANS Daniel <djans@cogitosum.com>
2024-11-01 08:30:50 -0400

    Update deploy.yml

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,11 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Congratulations Daniel et Clara et AMS 2001. You have successfully deployed the sample Spring Boot Application. </h1>";
+    return "<h1> Congratulations Daniel et Clara et AMS 2001. You have successfully deployed the sample Spring Boot Application. </h1>";
 }
+@RequestMapping("/home")
+public String home()
+    {
+        return "home";
+    }
 }
