d5fea46fd44a7ba3f45cee36b7bd94f60a584449
JANS Daniel <djans@cogitosum.com>
2024-11-12 15:22:04 -0500

    Correction after-install

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -5,14 +5,14 @@ running_containers=$(docker ps -q | wc -l)
 
 
 if [ "$running_containers" -eq 1 ]; then
-  # Log in to ECR
-  aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 227000603860.dkr.ecr.us-east-1.amazonaws.com
-  # Pull the Docker image from ECR
-  sudo docker pull 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
-  # Run the Docker container
-  sudo docker run -d -p 80:9080 -p 443:9443 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
+  echo "There is already $running_containers container running."
 else
-  echo "There are $running_containers containers running."
+    # Log in to ECR
+    aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 227000603860.dkr.ecr.us-east-1.amazonaws.com
+    # Pull the Docker image from ECR
+    sudo docker pull 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
+    # Run the Docker container
+    sudo docker run -d -p 80:9080 -p 443:9443 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
 fi
 
 rm /root/.aws/credentials
