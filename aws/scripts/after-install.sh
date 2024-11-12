1813c85a481170aabe7dbff2eac0ecf4edcdc89e
JANS Daniel <djans@cogitosum.com>
2024-11-12 15:14:23 -0500

    Correction after-install

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 set -xe
+# Get the number of running containers
+running_containers=$(docker ps -q | wc -l)
 
-# Log in to ECR
-aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 227000603860.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull the Docker image from ECR
-sudo docker pull 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
-
-# Run the Docker container
-sudo docker run -d -p 80:9080 -p 443:9443 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
+if [ "$running_containers" -eq 1 ]; then
+  # Log in to ECR
+  aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin 227000603860.dkr.ecr.us-east-1.amazonaws.com
+  # Pull the Docker image from ECR
+  sudo docker pull 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
+  # Run the Docker container
+  sudo docker run -d -p 80:9080 -p 443:9443 227000603860.dkr.ecr.us-east-1.amazonaws.com/cogitosum/ecr-01:latest
+else
+  echo "There are $running_containers containers running."
+fi
 
 rm /root/.aws/credentials
